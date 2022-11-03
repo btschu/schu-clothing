515fb35815467aba53598cf4f113b542bdc45502
Brandon Schumacher <92617960+btschu@users.noreply.github.com>
2022-11-02 21:03:48 -0600

    added sign-in route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./routes/home/home.component";
 
 import "./categories.styles.scss";
 import Navigation from "./routes/navigation/navigation.component";
+import SignIn from "./routes/sign-in/sign-in.component";
 
 const Shop = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
       <Route path='/' element={<Navigation/>}>
         <Route index element={<Home />}/>
         <Route path='shop' element={<Shop />}/>
+        <Route path='sign-in' element={<SignIn />}/>
       </Route>
     </Routes>
   );
