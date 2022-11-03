9332f17d133178d61b4c58ac0343b0282320d3ea
Brandon Schumacher <92617960+btschu@users.noreply.github.com>
2022-11-02 20:38:26 -0600

    added nav logo and links

--- src/App.js
+++ src/App.js
@@ -3,11 +3,23 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home.component";
 
 import "./categories.styles.scss";
+import Navigation from "./routes/navigation/navigation.component";
+
+const Shop = () => {
+  return (
+    <div>
+      <h1>I am the shop</h1>
+    </div>
+  )
+}
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />}/>
+      <Route path='/' element={<Navigation/>}>
+        <Route index element={<Home />}/>
+        <Route path='shop' element={<Shop />}/>
+      </Route>
     </Routes>
   );
 };
