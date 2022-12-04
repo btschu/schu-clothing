f68fd0e4f3728c293c095611ae0e22330ba8ca3a
Brandon Schumacher <92617960+btschu@users.noreply.github.com>
2022-12-03 20:26:23 -0700

    added categories

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import App from './App';
 // UserProvider tells us which components have access to context
 import {UserProvider} from './contexts/user.context.jsx'
-import {ProductsProvider} from './contexts/products.context.jsx'
+import {CategoriesProvider} from './contexts/categories.context.jsx'
 import {CartProvider} from './contexts/cart.context.jsx'
 
 import './index.scss';
@@ -16,11 +16,11 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <ProductsProvider>
+        <CategoriesProvider>
           <CartProvider>
             <App />
           </CartProvider>
-        </ProductsProvider>
+        </CategoriesProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
